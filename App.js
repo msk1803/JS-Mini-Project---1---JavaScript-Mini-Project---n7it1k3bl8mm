96eff42f777d96903ed2599c251775a873a77500
Shubham Kumar <97120264+msk1803@users.noreply.github.com>
2022-05-22 21:49:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,31 @@
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
 
+for (let item of buttons) {
+    // eslint-disable-next-line no-loop-func
+    item.addEventListener('click', (e)=>{
+       let buttonText = e.target.innerText;
+       
+        if (buttonText === 'X'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText === 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText === '=') {
+            // eslint-disable-next-line no-eval
+            screen.value = eval(screenValue);
+            
+            
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
